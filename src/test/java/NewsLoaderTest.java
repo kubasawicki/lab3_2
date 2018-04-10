90345c03e9395f0a06db0f0aa19a84559ad9a58e
Wiktor Pabich <202258@edu.p.lodz.pl>
2018-04-10 10:32:47 +0200

    Added tests for NewsLoader

--- src/test/java/NewsLoaderTest.java
+++ src/test/java/NewsLoaderTest.java
@@ -5,15 +5,21 @@ import jdk.nashorn.internal.runtime.regexp.joni.Config;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 
+import java.lang.reflect.Field;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({NewsReaderFactory.class, ConfigurationLoader.class})
+@PrepareForTest({NewsReaderFactory.class, ConfigurationLoader.class, PublishableNews.class})
 public class NewsLoaderTest {
 
     private ConfigurationLoader configLoader;
@@ -21,6 +27,9 @@ public class NewsLoaderTest {
     private Configuration config;
     private NewsReader reader;
     private IncomingNews incomingNews;
+    private IncomingNews onlyPublishableNews;
+    private PublishableNews publishable;
+    private Field field;
 
     @Before
     public void setup(){
@@ -29,16 +38,10 @@ public class NewsLoaderTest {
         newsLoader = new NewsLoader();
         reader = PowerMockito.mock(FileNewsReader.class);
         incomingNews = new IncomingNews();
+        onlyPublishableNews = new IncomingNews();
 
         PowerMockito.mockStatic(NewsReaderFactory.class);
         PowerMockito.mockStatic(ConfigurationLoader.class);
-
-        incomingNews.add(new IncomingInfo("incInfoA", SubsciptionType.A));
-        incomingNews.add(new IncomingInfo("incInfoB", SubsciptionType.B));
-        incomingNews.add(new IncomingInfo("incInfoC", SubsciptionType.C));
-        incomingNews.add(new IncomingInfo("incInfoNONE", SubsciptionType.NONE));
-
-        Whitebox.setInternalState(config, "readerType", config.getReaderType());
 
         PowerMockito.when(ConfigurationLoader.getInstance()).thenReturn(configLoader);
         PowerMockito.when(configLoader.loadConfiguration()).thenReturn(config);
@@ -46,8 +49,45 @@ public class NewsLoaderTest {
         PowerMockito.when(reader.read()).thenReturn(incomingNews);
 
     }
-//    @Test
-//    public void prepareForPublishShouldBeCalledOnlyOnce(){
-//
-//    }
+    @Test
+    public void resultIncommingNewsShouldContainCorrectNumberOfNews(){
+        incomingNews.add(new IncomingInfo("incInfoA", SubsciptionType.A));
+        incomingNews.add(new IncomingInfo("incInfoB", SubsciptionType.B));
+        incomingNews.add(new IncomingInfo("incInfoC", SubsciptionType.C));
+        incomingNews.add(new IncomingInfo("incInfoNONE", SubsciptionType.NONE));
+
+        IncomingNews resultIncomingNews = reader.read();
+        assertThat(resultIncomingNews.elems().size(), is(4));
+    }
+
+    @Test
+    public void loadNewsShouldReturnOnlyPublicNews(){
+        incomingNews.add(new IncomingInfo("incInfoA", SubsciptionType.A));
+        incomingNews.add(new IncomingInfo("incInfoB", SubsciptionType.B));
+        incomingNews.add(new IncomingInfo("incInfoC", SubsciptionType.C));
+        incomingNews.add(new IncomingInfo("incInfoNONE", SubsciptionType.NONE));
+
+        try {
+            field = PublishableNews.class.getDeclaredField("publicContent");
+            field.setAccessible(true);
+
+            List<String> result = (List<String>) field.get(newsLoader.loadNews());
+            assertThat(result.size(), is(1));
+        }catch(Exception e){
+            System.out.println("Casting to List exception");
+        }
+    }
+
+    @Test
+    public void loadNewsShouldReturnEmptyListWhenIncommingNewsEmpty(){
+        try {
+            field = PublishableNews.class.getDeclaredField("publicContent");
+            field.setAccessible(true);
+
+            List<String> result = (List<String>) field.get(newsLoader.loadNews());
+            assertThat(result.size(), is(0));
+        }catch(Exception e){
+            System.out.println("Casting to List exception");
+        }
+    }
 }
